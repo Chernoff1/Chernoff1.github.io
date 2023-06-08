e4869a71533f430adf8bb50172b8cc30a4bb54d8
Chernoff1 <82496951+Chernoff1@users.noreply.github.com>
2023-06-08 15:05:18 +0300

    asd

--- js/lab3/lab2.js
+++ js/lab3/lab2.js
@@ -1,77 +1,24 @@
-'use strict';
-/**
- * Возводит х в степень n 
- * @param {number} x основание степени
- * @param {number} n степень
- * @returns x  в степени n
- */
-function pow(x, n) {
-    return x ** n;
-}
-/**
- * Возвращает сумму от 1 до n
- * @param {number} n максимальное число
- * @returns сумму от 1 до n
- */
-function sumTo(n) {
-    let sum = 0;
-    for (let i = 1; i <= n; i++) {
-        sum += i
-    }
-    return sum;
-}
+'use strict'
 
 /**
- * Считает факториал числа n
- * @param {number} n основание факториала
- * @returns факториал  n
+ * Возвращает n-е число Фибоначчи.
+ *
+ * @param {number} n порядковый номер числа Фибоначчи.
+ * @return {number} b, n-е число Фибоначчи.
  */
-function factorial(n) {
-    if (n <= 1) return 1n;
-    else {
-        return BigInt(n) * factorial(n - 1);
-    }
-}
-/**
- * n-ый член ряда Фибоначчи
- * @param {number} n 
- * @returns  n-ый член ряда Фибоначчи
- */
-export function fib(n) {
-    let a = BigInt(1);
-    let b = BigInt(1);
-    if (n == BigInt(0)) {
-        return BigInt(0);
+
+export default function fib(n) {
+    let a = 1n;
+    let b = 1n;
+    if (n == 0n) {
+        return 0n;
     }
     else {
-        for (let i = BigInt(3); i <= n; i++) {
-           let c = a + b;
+        for (let i = 3n; i <= n; i++) {
+            let c = a + b;
             a = b;
             b = c;
         }
         return b;
     }
-}
-/**
- * Возвращает анонимную булеву функцию результата сравнения числа с x
- * @param {number} x число для сравнения
- * @returns возвращает анонимную булеву функцию результата сравнения числа с x
- */
-function compare(x) {
-    return function (y) {
-        if (y > x) return true;
-        else if (y < x) return false;
-        else if (y == x) return null;
-    }
-}
-/**
- * Сумма всех принятых аргументов функцией 
- * @returns сумму всех принятых аргументов функцией 
- */
-function sum() {
-    var result = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        result += arguments[i]
-    }
-    return result;
 }
--- js/lab3/lab3.js
+++ js/lab3/lab3.js
@@ -1,100 +1,133 @@
-'use strict';
+'use strict'
+
+import fib from './lab2.js'
+
 /**
- * Программа возвращает дробную часть числа
- * @param {number} num 
- * @returns дробную часть числа
+ * Программа возвращает дробную часть числа num.
+ *
+ * @param {number} num число, дробную часть которого нужно вернуть.
+ * @returns {function} результат выполнения методов, вычисляющие дробную часть числа num.
  */
-export function getDecimal(num) { //возвращает дробную часть числа num.
+
+export function getDecimal(num) {
     if (Math.trunc(num) == num) {
-        return 0;
+        return 0
     }
     else if (num < 0) {
-        return num - (-Math.ceil(-num));
+        return num - (-Math.ceil(-num))
     }
 
     if (num > 1) {
-        return +(num - Math.trunc(num)).toFixed(2);
+        return +(num - Math.trunc(num)).toFixed(2)
     }
 }
+
 /**
- * Функция  возвращает строку str с заглавным первым символом
- * @param {string} str 
- * @returns  возвращает строку str с заглавным первым символом
+ * Функция  возвращает строку str с заглавным первым символом.
+ *
+ * @param {string} str строка, первый символ которой нужно вернуть.
+ * @returns {string} возвращает строку str с заглавным первым символом.
  */
-export function ucFirst(str) { //возвращает строку str с заглавным первым символом.
+
+export function ucFirst(str) {
     if (!str) {
-        return str;
-    }
-    else {
-        return str[0].toUpperCase() + str.slice(1);
-    }
-}
-/**
- * Функция возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false
- * @param {string} str 
- * @returns возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false
- */
-export function checkSpam(str) { //возвращает true, если строка str содержит 'viagra' или 'XXX', а иначе false. Функция должна быть нечувствительна к регистру.
-    let newstr = str.toLowerCase();
-    return newstr.includes('viagra') || newstr.includes('xxx');
-}
-/**
- * Функция проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на символ многоточие "…"
- * @param {string} str 
- * @param {number} maxlength 
- * @returns отформатированная строка 
- */
-export function truncate(str, maxlength) { //проверяет длину строки str, и если она превосходит maxlength – заменяет конец str на символ многоточие "…"
-    if (str.length < maxlength) {
-        return str;
-    }
-    else {
-        return str.slice(0, maxlength - 1) + '…';
+        return str
+    } else {
+        return str[0].toUpperCase() + str.slice(1)
     }
 }
+
 /**
- *  Функция преобразует строку вида 'var-test-text' в 'varTestText'
- * @param {string} str 
- * @returns строка вида'varTestText'
+ * Функция возвращает true, если строка str содержит 'viagra' или 'XXX', в противном случае false.
+ *
+ * @param {string} str строка, которую необходимо проверить на "лишние" слова.
+ * @returns {bool} возвращает true, если строка str содержит 'viagra' или 'XXX', в противном случае false.
  */
-export function camelize(str) { //преобразуйте строку вида 'var-test-text' в 'varTestText'. То есть, дефисы удаляются, а все слова после них получают заглавную букву. 
-    let stroka = str.split("-");
-    let strnw = "";
-    for (let i = 0; i <= stroka.length - 1; i++) {
-        if (i == 0) strnw = stroka[0];
-        else strnw += ucFirst(stroka[i]);
-    }
-    return strnw;
+
+export function checkSpam(str) {
+    let new_str = str.toLowerCase()
+
+    return new_str.includes('viagra') || new_str.includes('xxx')
 }
+
 /**
- * Функция  возвращает массив, заполненный числами Фибоначчи до n-го (не включая его)
- * @param {number} n 
- * @returns  массив, заполненный числами Фибоначчи до n-го (не включая его)
+ * Функция проверяет длину строки str и, если она превосходит maxlength, заменяет конец str на символ многоточие "…".
+ *
+ * @param {string} str строка, которая проверяется на длину.
+ * @param {number} maxlength длина строки, которую нельзя превысить.
+ * @returns {string} результатом функции является отформатированная строка.
  */
-import { fib } from './lab2.js';
-export function fibs(n) {  //для натурального n возвращает массив, заполненный числами Фибоначчи до n-го (не включая его).
-    let mas = [];
-    for (let i = 0; i < n; i += 1) {
-        mas.push(fib(i));
+
+export function truncate(str, max_length) {
+    if (str.length < max_length) {
+        return str
+    } else {
+        return str.slice(0, max_length - 1) + '…'
     }
-    return mas;
 }
+
 /**
- * Функция возвращает массив из тех же элементов, но отсортированный по убыванию
- * @param {array} arr 
- * @returns  массив из тех же элементов, но отсортированный по убыванию
+ *  Функция удаляет из строки символ девиса '-'.
+ *
+ * @param {string} str строка, из которой необходимо удалить символ дефиса.
+ * @returns {string] new_str, строка без символа дефиса.
  */
- export function arrReverseSorted(arr) { //принимает неупорядоченный массив чисел arr и возвращает массив из тех же элементов, но отсортированный по убыванию
+
+export function camelize(str){
+    let buff_str = str.split("-")
+    let new_str = ""
+
+    for (let i = 0; i <= buff_str.length - 1; i++) {
+        if (i == 0) {
+            new_str = buff_str[0]
+        } else {
+            new_str += ucFirst(buff_str[i])
+        }
+    }
+
+    return new_str
+}
+
+/**
+ * Функция возвращает массив, заполненный числами Фибоначчи до n члена последовательности итальянского математика.
+ *
+ * @param {number} n необходимое количество членов последовательности ФиБоначчи.
+ * @returns {array}  mas, массив, заполненный члена последовательности итальянского математика.
+ */
+
+export function fibs(n){  
+        let mas = []
+        for (let i = 0; i < n; i++) {
+          mas.push(fib(i))
+        }
+
+        return mas
+}
+
+/**
+ * Функция возвращает отсортированный по убыванию массив.
+ *
+ * @param {array} arr массив, который необходимо отсортировать.
+ * @returns {array} отсортированный по убыванию массив, полученный в результате выполнения анониманой функции.
+ */
+
+export function  arrReverseSorted(arr) {
     let arr_copy = arr.slice();
+    
     return arr_copy.sort(function (a, b) {
-        return b - a;
+        return b - a
     })
 }
+
 /**
- * Функция массив уникальных, не повторяющихся значений массива arr
- * @param {array} arr 
- * @returns массив уникальных, не повторяющихся значений массива arr
+ * Функция возвращает измененный массив, состоящий из уникальных значений (аналог set).
+ *
+ * @param {array} arr массив, из которого нужно сделать множество.
+ * @returns {array} arr_copy, массив уникальных значений.
  */
-export function unique(arr) { //вернёт массив уникальных, не повторяющихся значений массива arr. Значения в массиве могут быть любого типа.
-    return Array.from(new Set(arr));
+
+export function unique(arr) {
+    let arr_copy = arr
+    
+    return Array.from(new Set(arr_copy))
 }
